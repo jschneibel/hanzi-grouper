0555d3a493553616e2703960d6723563bcaf55ca
jschneibel <jonathan.schneibel@gmail.com>
2019-09-12 21:46:38 +0200

    create hashmaps

--- hanzi-grouper/app/src/main/java/com/example/hanzi_grouper/Dictionary.java
+++ hanzi-grouper/app/src/main/java/com/example/hanzi_grouper/Dictionary.java
@@ -6,17 +6,21 @@ import java.io.*;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
-import java.util.Hashtable;
 
 public class Dictionary {
     private static Dictionary singleton;
 
-    private Hashtable<String, String> dictionary;
-    private ArrayList<String> simplified;
-    private ArrayList<String> traditional;
-    private ArrayList<String> pinyin;
-    private ArrayList<String> meaningsUnsplit;
-    private ArrayList<ArrayList<String>> meanings;
+
+    private HashMap<String, String> dictionary;
+    private HashMap<String, String> simplifiedToPinyin = new HashMap<String, String>();
+    private HashMap<String, String> simplifiedToMeaning = new HashMap<String, String>();
+
+    // Data of each column gets into its own ArrayList.
+    private ArrayList<String> simplified = new ArrayList<>();
+    private ArrayList<String> traditional = new ArrayList<>();
+    private ArrayList<String> pinyin = new ArrayList<>();
+    private ArrayList<String> meaningsUnsplit = new ArrayList<>();
+    private ArrayList<ArrayList<String>> meanings = new ArrayList<>();
 
     private Dictionary(InputStream dictionaryStream) {
         parseDictionaryFile(dictionaryStream);
@@ -31,9 +35,6 @@ public class Dictionary {
     }
 
     private void parseDictionaryFile(InputStream dictionaryStream) {
-        // index 0: hanzi
-        // index 1: pinyin
-        // index 2: meaning
 
         BufferedReader reader = new BufferedReader(new InputStreamReader(dictionaryStream));
 
@@ -45,30 +46,62 @@ public class Dictionary {
             while (reader.ready()) {
                 line = reader.readLine();
 
+                // Discard comments.
                 if (!line.startsWith("#")) {
                     delimited = line.split(" ", 2);
-                    simplified.add(delimited[0]);
 
-                    delimited = delimited[1].split(" ", 2);
-                    traditional.add(delimited[0]);
+                    // Only include single characters.
+                    if (delimited[0].length() == 1) {
+                        simplified.add(delimited[0]);
 
-                    delimited = delimited[1].split("] /", 2);
-                    pinyin.add(delimited[0]);
+                        delimited = delimited[1].split(" \\[", 2);
+                        traditional.add(delimited[0]);
 
-                    meaningsUnsplit.add(delimited[1]);
+                        delimited = delimited[1].split("\\] \\/", 2);
+                        pinyin.add(delimited[0]);
 
-                    delimited = delimited[1].split("/", 2);
-                    meanings.add(new ArrayList<String>(Arrays.asList(delimited)));
+                        // Remove trailing forward slash.
+                        delimited[1] = delimited[1].substring(0, delimited[1].length() - 1);
+                        meaningsUnsplit.add(delimited[1]);
+
+                        delimited = delimited[1].split("\\/");
+                        meanings.add(new ArrayList<String>(Arrays.asList(delimited)));
+                    }
                 }
             }
 
             reader.close();
+
+            for (int i = 0; i < simplified.size(); i++) {
+                if (simplified.get(i) == null ||
+                        pinyin.get(i) == null ||
+                        meaningsUnsplit.get(i) == null) {
+                    Log.d("custom", simplified.get(i));
+                    Log.d("custom", pinyin.get(i));
+                    Log.d("custom", meaningsUnsplit.get(i));
+                }
+                else {
+                    simplifiedToPinyin.put(simplified.get(i), pinyin.get(i));
+                    simplifiedToMeaning.put(simplified.get(i), meaningsUnsplit.get(i));
+                }
+            }
+
             Log.d("custom", "Length simplified: " + simplified.size());
             Log.d("custom", "Length traditional: " + traditional.size());
             Log.d("custom", "Length pinyin: " + pinyin.size());
             Log.d("custom", "Length meaningsUnsplit: " + meaningsUnsplit.size());
             Log.d("custom", "Length meanings: " + meanings.size());
-            Log.d("custom", "Finished parsing dictionary resource.");
+            Log.d("custom", "Finished parsing dictionary resource. \n");
+
+//            int id = 49982;
+            int id = 5002;
+            Log.d("custom", "Sample entry: ");
+            Log.d("custom", simplified.get(id));
+            Log.d("custom", traditional.get(id));
+            Log.d("custom", pinyin.get(id));
+            Log.d("custom", meaningsUnsplit.get(id));
+            Log.d("custom", meanings.get(id).toString());
+
         } catch (IOException e) {
             // TODO: catch or throw exception
             Log.d("custom", "Couldn't parse dictionary resource.");
