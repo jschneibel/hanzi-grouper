85b2998555e6cffa8c7a16e96932ccb1455dd244
jschneibel <37653547+jschneibel@users.noreply.github.com>
2019-09-12 23:47:51 +0200

    add RecyclerView with dummy data

--- hanzi-grouper/app/src/main/java/com/example/hanzi_grouper/MainActivity.java
+++ hanzi-grouper/app/src/main/java/com/example/hanzi_grouper/MainActivity.java
@@ -1,13 +1,24 @@
 package com.example.hanzi_grouper;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -26,6 +37,28 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        RecyclerView overviewRecycler = (RecyclerView) findViewById(R.id.overview_recycler);
+
+        // dummy data
+        ArrayList<String> group1 = new ArrayList<>();
+        group1.add("g1");
+        group1.add("h1");
+        group1.add("h2");
+        group1.add("h3");
+        ArrayList<String> group2 = new ArrayList<>();
+        group2.add("g2");
+        group2.add("h1");
+        group2.add("h2");
+        ArrayList<ArrayList<String>> groups = new ArrayList<>();
+        groups.add(group1);
+        groups.add(group2);
+
+        RecyclerView.Adapter overviewRecyclerAdapter = new OverviewRecyclerAdapter(groups);
+        overviewRecycler.setAdapter(overviewRecyclerAdapter);
+
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getBaseContext());
+        overviewRecycler.setLayoutManager(linearLayoutManager);
     }
 
     @Override
@@ -48,5 +81,49 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+}
+
+class OverviewRecyclerHolder extends RecyclerView.ViewHolder {
+    TextView group;
+
+    public OverviewRecyclerHolder(View v) {
+        super(v);
+
+        group = (TextView) v.findViewById(R.id.group);
+    }
+}
+
+class OverviewRecyclerAdapter extends RecyclerView.Adapter<OverviewRecyclerHolder> {
+    ArrayList<ArrayList<String>> groups;
+
+    OverviewRecyclerAdapter(ArrayList<ArrayList<String>> groups) {
+        this.groups = groups;
+    }
+
+    @Override
+    public int getItemCount() {
+        return groups.size();
+    }
+
+    @NonNull
+    @Override
+    public OverviewRecyclerHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.overview_layout, parent, false);
+        OverviewRecyclerHolder holder = new OverviewRecyclerHolder(v);
+        return holder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull OverviewRecyclerHolder overviewRecyclerHolder, int position) {
+        ArrayList<String> group = groups.get(position);
+
+        Log.d("test", groups.toString());
+        Log.d("test", group.toString());
+        String output = group.get(0).concat(":");
+        for (int i = 1; i < group.size(); i++) {
+            output = output.concat(" ").concat(group.get(i));
+        }
+        overviewRecyclerHolder.group.setText(output);
     }
 }
