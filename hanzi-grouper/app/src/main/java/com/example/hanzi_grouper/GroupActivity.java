ba21b85c047fdb7f064f66a76fb64f560f64f8c3
jschneibel <37653547+jschneibel@users.noreply.github.com>
2019-09-17 21:55:35 +0200

    receive intent from MainActivity

--- hanzi-grouper/app/src/main/java/com/example/hanzi_grouper/GroupActivity.java
+++ hanzi-grouper/app/src/main/java/com/example/hanzi_grouper/GroupActivity.java
@@ -1,5 +1,6 @@
 package com.example.hanzi_grouper;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -7,7 +8,11 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
+import java.util.ArrayList;
+
 public class GroupActivity extends AppCompatActivity {
+
+    private ArrayList<String> group;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,11 +21,16 @@ public class GroupActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        Intent intent = getIntent();
+        group = intent.getStringArrayListExtra(MainActivity.EXTRA_GROUP);
+
+        getSupportActionBar().setTitle(group.get(0));
+
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                Snackbar.make(view, group.get(0), Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
         });
